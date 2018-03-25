1f7dc03e4b0ad9f68e73f80fe7f92717660c16f2
Kada Benadjemia <kada.benadjemia@tu-dortmund.de>
2018-03-25 23:40:58 +0200

    initial commit

--- graphs.py
+++ graphs.py
@@ -5,8 +5,7 @@ from sugiyama import *
 
 import gi
 gi.require_version('Gtk', '3.0')
-gi.require_version('PangoCairo', '1.0')
-from gi.repository import Gtk, Gdk, Pango, PangoCairo
+from gi.repository import Gtk, Gdk
 
 import cairo
 
